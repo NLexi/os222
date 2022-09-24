0940e50f54d23801482ddbc23980a40292c76e51
NLexi <96821857+NLexi@users.noreply.github.com>
2022-09-24 23:02:58 +0700

    commit

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -12,7 +12,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="5029FF49D6596E4D"
+REC2="0935E6BAB549578E"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
